8ed4cc585aae41884b0aa88df2ace647445b3897
devolps-21 <survivecan22@gmail.com>
2025-12-18 05:12:28 -0800

    Create WeatherService.java

---
+++ WeatherService.java
@@ -0,0 +1,9 @@
+package server;
+
+public class WeatherService {
+
+    public static String getWeather(String city) {
+        // Dummy data (later you can connect API)
+        return "🌤 Weather in " + city + ": 25°C, Sunny";
+    }
+}
